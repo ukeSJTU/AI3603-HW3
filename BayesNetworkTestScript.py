a0fa2292f6f85f91f5fa9309d0e9696e806d77b8
ukeSJTU <uke_sjtu@sjtu.edu.cn>
2025-11-24 16:15:19 +0800

    chore: copy given starter code

---
+++ BayesNetworkTestScript.py
@@ -0,0 +1,73 @@
+# -*- coding:utf-8 -*-
+
+from BayesianNetworks import *
+import numpy as np
+import pandas as pd
+
+#############################
+## Example Tests from Bishop `Pattern Recognition and Machine Learning` textbook on page 377
+#############################
+BatteryState = readFactorTable(['battery'], [0.9, 0.1], [[1, 0]])
+FuelState = readFactorTable(['fuel'], [0.9, 0.1], [[1, 0]])
+GaugeBF = readFactorTable(['gauge', 'battery', 'fuel'], [0.8, 0.2, 0.2, 0.1, 0.2, 0.8, 0.8, 0.9], [[1, 0], [1, 0], [1, 0]])
+
+carNet = [BatteryState, FuelState, GaugeBF]  # carNet is a list of factors
+## Notice that different order of operations give the same answer
+## (rows/columns may be permuted)
+joinFactors(joinFactors(BatteryState, FuelState), GaugeBF)
+joinFactors(joinFactors(GaugeBF, FuelState), BatteryState)
+
+marginalizeFactor(joinFactors(GaugeBF, BatteryState), 'gauge')
+joinFactors(marginalizeFactor(GaugeBF, 'gauge'), BatteryState)
+
+joinFactors(marginalizeFactor(joinFactors(GaugeBF, BatteryState), 'battery'), FuelState)
+marginalizeFactor(joinFactors(joinFactors(GaugeBF, FuelState), BatteryState), 'battery')
+
+marginalizeFactor(joinFactors(marginalizeFactor(joinFactors(GaugeBF, BatteryState), 'battery'), FuelState), 'gauge')
+marginalizeFactor(joinFactors(marginalizeFactor(joinFactors(GaugeBF, BatteryState), 'battery'), FuelState), 'fuel')
+
+evidenceUpdateNet(carNet, ['fuel', 'battery'], [1, 0])
+
+# inference
+print("inference starts")
+print(inference(carNet, ['battery', 'fuel'], [], []))  ## chapter 8 equation (8.30)
+print(inference(carNet, ['battery'], ['fuel'], [0]))  ## chapter 8 equation (8.31)
+print(inference(carNet, ['battery'], ['gauge'], [0]))  ##chapter 8 equation  (8.32)
+print(inference(carNet, [], ['gauge', 'battery'], [0, 0]))  ## chapter 8 equation (8.33)
+print("inference ends")
+###########################################################################
+# RiskFactor Data Tests
+###########################################################################
+riskFactorNet = pd.read_csv('RiskFactorsData.csv')
+
+# Create factors
+
+income = readFactorTablefromData(riskFactorNet, ['income'])
+smoke = readFactorTablefromData(riskFactorNet, ['smoke', 'income'])
+exercise = readFactorTablefromData(riskFactorNet, ['exercise', 'income'])
+long_sit = readFactorTablefromData(riskFactorNet, ['long_sit', 'income'])
+stay_up = readFactorTablefromData(riskFactorNet, ['stay_up', 'income'])
+bmi = readFactorTablefromData(riskFactorNet, ['bmi', 'income'])
+diabetes = readFactorTablefromData(riskFactorNet, ['diabetes', 'bmi'])
+
+## you need to create more factor tables
+
+risk_net = [income, smoke, long_sit, stay_up, exercise, bmi, diabetes]
+print("income dataframe is ")
+print(income)
+factors = riskFactorNet.columns
+
+# example test p(diabetes|smoke=1,exercise=2,long_sit=1)
+
+margVars = list(set(factors) - {'diabetes', 'smoke', 'exercise', 'long_sit'})
+obsVars = ['smoke', 'exercise', 'long_sit']
+obsVals = [1, 2, 1]
+
+p = inference(risk_net, margVars, obsVars, obsVals)
+print(p)
+
+
+###########################################################################
+# Please write your own test script
+# HW4 test scripts start from here
+###########################################################################
